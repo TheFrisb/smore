88700380e27889b06222a18de1a8cd2152414029
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-03-28 18:27:31 +0100

    R

--- core/management/commands/test.py
+++ core/management/commands/test.py
@@ -1,27 +1,47 @@
+from datetime import datetime, timezone
+
 from django.core.management.base import BaseCommand
 
-from core.models import SportTeam, Product, ApiSportModel
+from core.models import ApiSportModel, Product
+from core.services.basketball_api_service import BasketballApiService
+from core.services.football_api_service import FootballApiService
+from core.services.hocker_api_service import HockeyApiService
+from core.services.sport_api_service import SportApiService
 
 
 class Command(BaseCommand):
     help = "Create 20 FAQ items with varying questions and answers."
 
     def handle(self, *args, **kwargs):
-        sport_teams = SportTeam.objects.all().prefetch_related("league", "league__product", "product")
-        soccer = Product.objects.get(name=Product.Names.SOCCER)
-        basketball = Product.objects.get(name=Product.Names.BASKETBALL)
+        sport_api_service = SportApiService()
 
-        for team in sport_teams:
-            print(f"Processing team: {team.name}, product: {team.product}, league: {team.product.name}")
-            if team.league.product != team.product:
-                print(
-                    f"Team {team.name} has different product ({team.product}) than league's: {team.league.product}"
-                )
-                team.product = team.league.product
+        sport_api_service.populate_countries(ApiSportModel.SportType.SOCCER)
+        sport_api_service.populate_countries(ApiSportModel.SportType.BASKETBALL)
+        sport_api_service.populate_countries(ApiSportModel.SportType.NFL)
 
-                if team.league.product == soccer:
-                    team.type = ApiSportModel.SportType.SOCCER
-                elif team.league.product == basketball:
-                    team.type = ApiSportModel.SportType.BASKETBALL
+        sport_api_service.populate_leagues(
+            ApiSportModel.SportType.SOCCER,
+            Product.objects.get(name=Product.Names.SOCCER),
+        )
 
-                team.save()
+        sport_api_service.populate_leagues(
+            ApiSportModel.SportType.BASKETBALL,
+            Product.objects.get(name=Product.Names.BASKETBALL),
+        )
+
+        sport_api_service.populate_leagues(
+            ApiSportModel.SportType.NFL,
+            Product.objects.get(name=Product.Names.NFL_NHL_NCAA),
+        )
+
+    def load_matches(self):
+        start_date = datetime(2025, 3, 28, tzinfo=timezone.utc)
+        end_date = datetime(2025, 4, 3, tzinfo=timezone.utc)
+
+        basketball_api_service = BasketballApiService()
+        football_api_service = FootballApiService()
+        hockey_api_service = HockeyApiService()
+
+        football_api_service.populate_matches(start_date, end_date)
+        hockey_api_service.populate_matches(start_date, end_date)
+        basketball_api_service.populate_matches(start_date, end_date)
