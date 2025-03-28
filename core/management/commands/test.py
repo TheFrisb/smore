c09300bf585aa0db900b09f73568864442626138
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-03-28 18:32:53 +0100

    R

--- core/management/commands/test.py
+++ core/management/commands/test.py
@@ -2,7 +2,6 @@ from datetime import datetime, timezone
 
 from django.core.management.base import BaseCommand
 
-from core.models import ApiSportModel, Product
 from core.services.basketball_api_service import BasketballApiService
 from core.services.football_api_service import FootballApiService
 from core.services.hocker_api_service import HockeyApiService
@@ -14,25 +13,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         sport_api_service = SportApiService()
-
-        sport_api_service.populate_countries(ApiSportModel.SportType.SOCCER)
-        sport_api_service.populate_countries(ApiSportModel.SportType.BASKETBALL)
-        sport_api_service.populate_countries(ApiSportModel.SportType.NHL)
-
-        sport_api_service.populate_leagues(
-            ApiSportModel.SportType.SOCCER,
-            Product.objects.get(name=Product.Names.SOCCER),
-        )
-
-        sport_api_service.populate_leagues(
-            ApiSportModel.SportType.BASKETBALL,
-            Product.objects.get(name=Product.Names.BASKETBALL),
-        )
-
-        sport_api_service.populate_leagues(
-            ApiSportModel.SportType.NHL,
-            Product.objects.get(name=Product.Names.NFL_NHL_NCAA),
-        )
+        self.load_matches()
 
     def load_matches(self):
         start_date = datetime(2025, 3, 28, tzinfo=timezone.utc)
