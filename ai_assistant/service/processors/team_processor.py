1f5c387c7a5bc07734b87720bfc7acfcd9211010
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-04-06 03:23:07 +0200

    R

--- ai_assistant/service/processors/team_processor.py
+++ ai_assistant/service/processors/team_processor.py
@@ -56,7 +56,7 @@ class TeamProcessor(BaseProcessor):
 
         if (
                 prompt_type in self.get_match_related_prompt_types()
-                and not prompt_context.team_names
+                and not prompt_context.team_names and not prompt_context.leagues
         ):
             logger.info(
                 f"No team names found in prompt context: {prompt_context}. Assuming that random teams are needed."
