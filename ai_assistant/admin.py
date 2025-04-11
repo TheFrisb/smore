8f73d5ab2e746c45c06e8760e59b1fc5d2236cfb
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-04-11 02:15:05 +0200

    R

--- ai_assistant/admin.py
+++ ai_assistant/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from ai_assistant.models import Message
 
+
 # Register your models here.
-admin.site.register(Message)
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("message", "direction", "user", "created_at")
+    list_filter = ("direction", "user")
+    search_fields = ("message", "user__username")
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
