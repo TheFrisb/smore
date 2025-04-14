4ac1295f00105d3284be585f00024b48277bbe99
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-04-14 18:02:24 +0200

    R

--- core/management/commands/test.py
+++ core/management/commands/test.py
@@ -1,26 +1,53 @@
-from datetime import datetime, timezone
-
 from django.core.management.base import BaseCommand
 
-from core.services.basketball_api_service import BasketballApiService
-from core.services.football_api_service import FootballApiService
-from core.services.sport_api_service import SportApiService
+from core.models import SportLeague, Product
+from core.services.basketball_api_service import BASKETBALL_NCAA_LEAGUE_IDS
 
 
 class Command(BaseCommand):
     help = "Create 20 FAQ items with varying questions and answers."
 
+    #
+    # def handle(self, *args, **kwargs):
+    #     sport_api_service = SportApiService()
+    #
+    #     self.load_matches()
+    #
+    # def load_matches(self):
+    #     start_date = datetime(2025, 3, 1, tzinfo=timezone.utc)
+    #     end_date = datetime(2025, 4, 13, tzinfo=timezone.utc)
+    #
+    #     football_api_service = FootballApiService()
+    #     basketball_api_service = BasketballApiService()
+    #
+    #     # basketball_api_service.populate_matches(start_date, end_date)
+    #     football_api_service.populate_matches(start_date, end_date)
+
     def handle(self, *args, **kwargs):
-        sport_api_service = SportApiService()
+        leagues = SportLeague.objects.filter(external_id__in=BASKETBALL_NCAA_LEAGUE_IDS)
 
-        self.load_matches()
+        for league in leagues:
+            print(
+                f"League product name: {league.product.name} - League type: {league.type} - League name: {league.name}"
+            )
 
-    def load_matches(self):
-        start_date = datetime(2025, 3, 1, tzinfo=timezone.utc)
-        end_date = datetime(2025, 4, 13, tzinfo=timezone.utc)
+            if league.type == SportLeague.SportType.BASKETBALL:
+                print(f"Updating the league's product to BASKETBALL")
+                league.product = Product.objects.get(name=Product.Names.BASKETBALL)
+                league.save()
+            elif (
+                    league.type == SportLeague.SportType.NHL
+                    or league.type == SportLeague.SportType.NFL
+            ):
+                print(f"Updating the league's product to NHL_NFL")
+                league.product = Product.objects.get(name=Product.Names.NFL_NHL)
+                league.save()
 
-        football_api_service = FootballApiService()
-        basketball_api_service = BasketballApiService()
+            elif league.type == SportLeague.SportType.SOCCER:
+                print(f"Updating the league's product to SOCCER")
+                league.product = Product.objects.get(name=Product.Names.SOCCER)
+                league.save()
 
-        # basketball_api_service.populate_matches(start_date, end_date)
-        football_api_service.populate_matches(start_date, end_date)
+            print(
+                f"League product name: {league.product.name} - League type: {league.type} - League name: {league.name}"
+            )
