2aeb07a25908de890d0ec1d60db0854c34b75ae1
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-07-17 06:30:06 -0700

    R

--- ai_assistant/service/processors/team_processor.py
+++ ai_assistant/service/processors/team_processor.py
@@ -99,7 +99,7 @@ class TeamProcessor(BaseProcessor):
         if filter_by_leagues:
             logger.info(f"Filtering by leagues: {filter_by_leagues}")
             base_queryset = base_queryset.filter(
-                leagues__external_id__in=filter_by_leagues
+                league__external_id__in=filter_by_leagues
             )
         else:
             base_queryset = base_queryset.filter(
