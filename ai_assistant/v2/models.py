8484325a95e880162e558b09f393cea163da157c
Aleksandar Bedjovski <aleksandar.bedjovski@internetbrands.com>
2025-08-12 07:02:47 +0200

    R

--- ai_assistant/v2/models.py
+++ ai_assistant/v2/models.py
@@ -1,6 +1,4 @@
 from django.conf import settings
 from langchain_openai import ChatOpenAI
 
-openai_o3_model = ChatOpenAI(
-    model="gpt-5-2025-08-07", api_key=settings.OPENAI_API_KEY
-)
+openai_o3_model = ChatOpenAI(model="gpt-5-2025-08-07", api_key=settings.OPENAI_API_KEY)
